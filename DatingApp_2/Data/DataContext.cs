d4c413eb690f3dfa248846f329b7535bbed51214
elprs <eleni.parisi.sunshine@gmail.com>
2021-01-19 19:30:24 +0200

    init

---
+++ DatingApp_2/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp_2.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp_2.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext( DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
